b67de62c09f486befce1fa870a18cd43eeb2b32d
Matthew Schauer <matthew.schauer@e10x.net>
2019-08-18 14:59:34 -0700

    Add announcement view

---
+++ src/components/officer/announcements.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="announcements">
+		Announcements
+	</div>
+</template>
+
+<script>
+import common from "@/common"
+
+export default {
+	name: "announcements",
+	data() {
+		return {
+			common: common,
+		}
+	},
+	mounted() {
+	}
+}
+</script>
